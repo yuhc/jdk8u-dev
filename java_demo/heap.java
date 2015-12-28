13cf9502cdb1d4c3f70fdcfdf9098f44d9dde228
yuhc <yuhc123@gmail.com>
2015-12-28 15:48:51 -0600

    Add a demo which requires 12 MB heap space.

---
+++ java_demo/heap.java
@@ -0,0 +1,11 @@
+class heap {
+
+    // run with -Xmx12m , it will return 
+    // java.lang.OutOfMemoryError: Java heap space
+    // it will be fine if run with -Xmx13m
+    static final int SIZE=2*1024*1024;
+
+    public static void main(String[] a) {
+        int[] i = new int[SIZE];
+     }
+}
