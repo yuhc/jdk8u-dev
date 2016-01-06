cb5e9d9e7abee9c677e539575c89d18633dea477
Hangchen YU <yuhc123@gmail.com>
2016-01-05 21:29:24 -0600

    Add parameters to test script

--- java_demo/spec/specjvm/run-simple.sh
+++ java_demo/spec/specjvm/run-simple.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
-java -XX:+UseParallelGC -Xbootclasspath/p:lib/javac.jar -jar -XX:+PrintGCDetails SPECjvm2008.jar -ikv -wt 5s -it 5s -bt 4 compress
+if [ $# = 0 ]; then
+    ALG="ParallelGC"
+    LOG=""
+elif [ $# = 1 ]; then
+    if [ $1 = 'p' ]; then
+        ALG="ParallelGC"
+    elif [ $1 = 'g' ]; then
+        ALG="G1GC"
+    fi
+    LOG=""
+elif [ $# = 2 ]; then
+    if [ $1 = 'p' ]; then
+        ALG="ParallelGC"
+    elif [$1 = 'g' ]; then
+        ALG="G1GC"
+    fi
+    if [ $2 = 'y' ]; then
+        LOG="-XX:+PrintGCDetails"
+    else
+        LOG=""
+    fi
+fi
+
+java -XX:+Use${ALG} ${LOG} -Xbootclasspath/p:lib/javac.jar -jar SPECjvm2008.jar -ikv -wt 5s -it 5s -bt 4 compress
