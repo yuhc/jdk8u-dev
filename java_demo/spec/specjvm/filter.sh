172ded5b09f2128002c778b72ed09b8e62e69c35
yuhc <yuhc123@gmail.com>
2016-01-14 13:48:31 -0600

    Update filter to fractor the result

--- java_demo/spec/specjvm/filter.sh
+++ java_demo/spec/specjvm/filter.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 SPACE='eden space'
+EDEN='eden space'
+OLD='object space'
 
 # ${1} should be the name of the directory
 COUNT=1
-for file in ${1}/scimark*
+for file in ${1}/derby*
 do
-    echo -n ${COUNT}:
-    COUNT=$((COUNT+1))
-    echo ${file} | sed 's/^.*\///'
-    grep ${file} -e "result:.*ops" | sed 's/^.*result:\s//'
+    #echo -n ${COUNT}:
+    #COUNT=$((COUNT+1))
+    #echo ${file} | sed 's/^.*\///'
 
-    grep ${file} -e "> ${SPACE}" | sed 's/^.*space\s\+\([0-9]\+\)K.*/\1/' \
-        | awk '{s+=$1; n+=1} END {print s/n,"K"}'
+    grep ${file} -e "result:.*ops" | sed 's/^.*result:\s\(.*\)/\1/'
+
+    grep ${file} -e "> ${EDEN}" | sed 's/^.*space\s\+\([0-9]\+\)K.*/\1/' \
+        | awk 'BEGIN{mn=1000000}{if (mn>$1) {mn=$1} if (mx<$1) {mx=$1} s+=$1; n+=1} END {print "EDEN => MIN:",mn,"K"," AVG:",s/n,"K"," MAX:",mx,"K"}'
+    grep ${file} -e "> ${OLD}" | sed 's/^.*space\s\+\([0-9]\+\)K.*/\1/' \
+        | awk 'BEGIN{mn=1000000}{if (mn>$1) {mn=$1} if (mx<$1) {mx=$1} s+=$1; n+=1} END {print "OLD  => MIN:",mn,"K"," AVG:",s/n,"K"," MAX:",mx,"K"}'
 done
